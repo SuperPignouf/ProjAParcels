5aa2d26d107802b1f1c2581bd2ef5203664ba397
auplisni <auplisni@ulb.ac.be>
2014-03-27 18:19:48 +0100

    Android: connection au WebService work in progress

--- ParcelsScanner/src/com/example/parcelsscanner/MainActivity.java
+++ ParcelsScanner/src/com/example/parcelsscanner/MainActivity.java
@@ -1,27 +1,37 @@
 package com.example.parcelsscanner;
 
+import rest.RestClient;
+import rest.RestClient.RequestMethod;
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	Button submit = null;
+	private Button submit = null;
 
-	EditText matricule = null;
-	EditText password = null;
+	private EditText matricule = null;
+	private EditText password = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+		boolean isConnected = networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
+		if (isConnected) System.out.println("Internet available");
+		else System.out.println("No Internet");
+			//boolean wifi = networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+			//Log.d("NetworkState", "L'interface de connexion active est du Wifi : " + wifi);
+		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
@@ -40,8 +50,18 @@ public class MainActivity extends Activity {
 	// Uniquement pour le bouton "envoyer"
 	private OnClickListener envoyerListener = new OnClickListener() {
 		@Override
-		public void onClick(View v) {
-			// TODO: verification de l'identite aupres de la DB
+		public void onClick(View v) {		
+			RestClient client =  new RestClient("http://localhost:8080/ParcelREST/rest/hello");
+			client.AddParam("matricule", matricule.getText().toString());
+			client.AddParam("password", password.getText().toString());
+			client.setRequestType(RequestMethod.GET);
+			try {
+			    client.execute();
+			} catch (Exception e) {
+			    e.printStackTrace();
+			}
+
+			String response = client.getResponse();
 		}
 	};
 }
